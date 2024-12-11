268cdb388aea8b9eb8f4d8541e0857362da87733
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-12-11 14:30:05 +0200

    inital commit

---
+++ src/server/db/index.ts
@@ -0,0 +1,18 @@
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+
+import { env } from "@/env";
+
+import * as schema from "./schema";
+
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: typeof sql | undefined;
+};
+const conn = globalForDb.conn ?? sql;
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn, { schema });
